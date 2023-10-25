2649eaa9ea4d3f451f0bd30067a0d72130a8bab4
IsmaelMV <maelsurf2005@gmail.com>
2023-10-25 12:36:52 -0300

    Add files via upload

---
+++ Wrappers2.java
@@ -0,0 +1,36 @@
+/**
+ * @author Ismael Alves
+ */
+
+package br.com.rpires;
+
+public class Wrappers2 {
+
+    public static void main(String args[]){
+
+        //primativo int
+        int numero = 10;
+
+        //Wrappers Integer
+        Integer numero1 = (numero);
+
+        //primativo long
+        long numero2 = (19851985l);
+
+        //primativo Long
+        Long numero3 = (numero2);
+
+        //primativo float
+        float numero4 = (1.4f);
+
+        //primativo Float
+        Float numero5 = (numero4);
+
+        System.out.println(numero);
+
+        System.out.println(numero2);
+
+        System.out.println(numero4);
+    }
+
+}
